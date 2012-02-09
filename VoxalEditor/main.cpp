6edbbca022d8e7c80a25d7fecbfc0502bf5f9c90
Vbitz <jscarsbrook@gmail.com>
2012-02-09 14:55:34 +1300

    Initial Commit

---
+++ VoxalEditor/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VoxalEditor
+//
+//  Created by Joshua Scarsbrook on 9/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
